8ef4b7eceda17e59066845077a9efbee0a6ec8f0
Juan Bermudez <juanbermudez_10@ieee.org>
2024-03-02 20:42:43 -0500

    create files and directories

---
+++ Interfaz_prueba/src/interfaz_prueba/Interfaz_prueba.java
@@ -0,0 +1,14 @@
+package interfaz_prueba;
+/**
+ * @author juanbermudezg
+ */
+public class Interfaz_prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
